fcafc7eaba03ff2cd3f2274ea2610ffc6756ec31
Alexander Taylor <ajtaylor@fuzyll.com>
2017-07-08 22:32:46 -0400

    Added license headers to each file.

--- extract.rb
+++ extract.rb
@@ -1,5 +1,29 @@
 #!/usr/bin/env ruby
 
+##
+# extract.rb | Wrastor Extraction Utility
+#
+# Copyright (c) 2017 Alexander Taylor <ajtaylor@fuzyll.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+##
+
 # strip the GameMaker archive out of the executable
 open("RivalsofAether.exe", "rb") do |file|
     data = file.read()  # FIXME: reading in the entire file here is not the most elegant solution...
